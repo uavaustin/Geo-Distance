f1598e6c468dc5199741fe276d98fa4a806ed80b
Rahul Butani <rr.butani@gmail.com>
2016-09-25 02:37:51 -0500

    Fixed circular import thing + other misc fixes/changes
    I ended up pretty much just dodging the issue by putting Distance and Distance3D in the same class since:
    	a) that seems way more elegant
    	b) it's reasonably pythonic and PEP8 compliant (allegedly - according to SO)
    
    Anyways, add, str, and sub in Distance seem to work.

--- geo_distance/distance.py
+++ geo_distance/distance.py
@@ -4,13 +4,12 @@ between two points.
 
 from math import atan2, sin, cos, sqrt, pi, copysign
 
-
 class Distance(object):
 
     """Represents a distance between two points in vector form.
 
-    Two distances can be added together and distances can be transformed
-    by an angle.
+    Two distances can be added or subtracted and distances can be 
+    transformed by an angle.
     """
 
     def __init__(self, x, y):
@@ -19,12 +18,13 @@ class Distance(object):
         self.y = y
 
     def __add__(self, dist):
-        """Add two Distance objects together and return a new
-        Distance.
+        """Add two Distance* objects together and return a new
+        Distance*.
         """
         x = self.x + dist.x
         y = self.y + dist.y
         z = 0
+
         if (isinstance(self, Distance3D)):
             z += self.z
         if (isinstance(dist, Distance3D)):
@@ -36,13 +36,15 @@ class Distance(object):
         return Distance(x,y)
 
     def __sub__(self, dist):
-        """Subtract a Distance object and return a new Distance."""
+        """Subtract a Distance* object from another Distance* object
+        and return a new Distance*.
+        """
         x = self.x - dist.x
         y = self.y - dist.y
         z = 0
 
         if (isinstance(self, Distance3D)):
-            z -= self.z
+            z += self.z
         if (isinstance(dist, Distance3D)):
             z -= dist.z
 
@@ -52,8 +54,8 @@ class Distance(object):
         return Distance(x, y)
 
     def __str__(self):
-        """Print detail of a Distance object."""
-        return ("x: %s, y: %s" % (self.x,self.y))
+        """Print the components of a Distance object."""
+        return ('x: {0.x:d}, y: {0.y:d}'.format(self))
 
     def get_magnitude(self):
         """Return the magnitude of the Distance object from its
@@ -77,5 +79,44 @@ class Distance(object):
         """Return a new Distance object from a magnitude in the xy plane
         and a bearing.
         """
-
+        #TODO: Since this is static, code for 3D here?
         return Distance(dist * sin(bearing), dist * cos(bearing), 0)
+
+class Distance3D(Distance):
+
+    """Subclass of Distance with an additional component (z)"""
+
+    def __init__(self, x, y, z):
+        """Instantiate a Distance3D object with components x, y, and z."""
+        super(Distance3D, self).__init__(x, y)
+        self.z = z
+
+    def __str__(self):
+        """Print the components of a Distance3D object."""
+        return ('x: {0.x:d}, y: {0.y:d} z: {0.z:d}'.format(self))
+
+    def get_magnitude(self):
+        """Return the magnitude of the Distance3D object from its
+        components.
+        """
+
+        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def get_transform(self, angle):
+        """Transform the Distance3D object by an angle clockwise and
+        return a new Distance3D object.
+        """
+
+        x = self.x * cos(angle) - self.y * sin(angle)
+        y = self.x * sin(angle) + self.y * cos(angle)
+        z = self.z #TODO?
+
+        return Distance3D(x, y, z)
+
+    @staticmethod
+    def from_magnitude(dist, bearing):
+        """Return a new Distance object from a magnitude in the xy plane
+        and a bearing.
+        """
+        #TODO: Need to be modified for 3D?
+        return Distance3D(dist * sin(bearing), dist * cos(bearing), 0)
