281964675eb37a9c76029e6b8f2f91d38573c3a7
bloof-bb <bloof.bb@gmail.com>
2016-10-03 01:28:38 -0500

    Tighten up distance.py

--- geo_distance/distance.py
+++ geo_distance/distance.py
@@ -1,11 +1,11 @@
-"""Contains the Distance class that represents distances in vector form
-between two points.
+"""Contains the Distance and Distance3D classes that represent
+distances in vector form between two points.
 """
 
-from math import atan2, sin, cos, sqrt, pi, copysign
+from math import atan2, sin, cos, sqrt, pi
 
-class Distance(object):
 
+class Distance:
     """Represents a distance between two points in vector form.
 
     Two distances can be added or subtracted and distances can be
@@ -13,20 +13,18 @@ class Distance(object):
     """
 
     def __init__(self, x, y):
-        """Instantiate a Distance object with components x and y.
-        (in meters)
-        """
+        """Instantiate a Distance object with components x and y."""
 
         self.x = x
         self.y = y
 
     def __add__(self, dist):
-        """Add two Distance* objects together and return a new
-        Distance*.
+        """Add two Distance objects together and return a new
+        Distance.
         """
         if not isinstance(dist, Distance):
-            raise TypeError('Cannot add ', type(dist),
-                ' to a Distance/Distance3D object.')
+            raise TypeError('Cannot add', type(dist),
+                'to a', type(self), 'object.')
 
         x = self.x + dist.x
         y = self.y + dist.y
@@ -34,6 +32,7 @@ class Distance(object):
 
         if isinstance(self, Distance3D):
             z += self.z
+
         if isinstance(dist, Distance3D):
             z += dist.z
 
@@ -43,32 +42,23 @@ class Distance(object):
         return Distance(x,y)
 
     def __sub__(self, dist):
-        """Subtract a Distance* object from another Distance* object
-        and return a new Distance*.
+        """Subtract a Distance object from another Distance object
+        and return a new Distance.
         """
         if not isinstance(dist, Distance):
-            raise TypeError('Cannot subtract ', type(dist),
-                ' from a Distance/Distance3D object.')
+            raise TypeError('Cannot subtract', type(dist),
+                'from a', type(self), 'object.')
 
-        x = self.x - dist.x
-        y = self.y - dist.y
-        z = 0
-
-        if isinstance(self, Distance3D):
-            z += self.z
-        if isinstance(dist, Distance3D):
-            z -= dist.z
-
-        if isinstance(self, Distance3D) or isinstance(dist, Distance3D):
-            return Distance3D(x, y, z)
-
-        return Distance(x, y)
+        return self + -1 * dist
 
     def __mul__(self, scalar):
-        x = self.x * scalar #TODO
+        """Multiply a Distance object by a scalar and return a new
+        Distance object.
+        """
+        x = self.x * scalar
         y = self.y * scalar
 
-        if isinstance(self, Distance):
+        if not isinstance(self, Distance3D):
             return Distance(x, y)
 
         z = self.z * scalar
@@ -76,34 +66,31 @@ class Distance(object):
         return Distance3D(x, y, z)
 
     def __rmul__(self, scalar):
-        return __mul__(self, scalar)
+        """Pre-multiply a Distance object by a scalar and return a
+        new Distance object.
+        """
+        return self * scalar
 
     def __div__(self, scalar):
-        return __mul__(self, 1.0 / scalar)
-
-    def __rdiv__(self, scalar):
-        return __div__(self, scalar)
-
-    def __str__(self):
-        """Return the components of a Distance object."""
-        return ('x: {0.x:f}, y: {0.y:f}'.format(self))
+        """Divide a Distance object by a scalar and return a new
+        Distance object.
+        """
+        return self * (1 / scalar)
 
     def get_magnitude(self):
         """Return the magnitude of the Distance object from its
         components.
         """
-
         return sqrt(self.x ** 2 + self.y ** 2)
 
     def get_bearing(self):
-
+        """Return the bearing of the Distance object."""
         return atan2(self.x, self.y) % (2 * pi)
 
     def get_transform(self, angle):
         """Transform the Distance object by an angle clockwise and
         return a new Distance.
         """
-
         x = self.x * cos(angle) - self.y * sin(angle)
         y = self.x * sin(angle) + self.y * cos(angle)
 
@@ -117,41 +104,48 @@ class Distance(object):
 
         return cls(dist * sin(bearing), dist * cos(bearing))
 
-class Distance3D(Distance):
+    def __str__(self):
+        """Return the components of a Distance object in a string.
+        """
+        return ('x: {0.x:f}, y: {0.y:f}'.format(self))
 
-    """Subclass of Distance with an additional component (z)."""
+
+class Distance3D(Distance):
+    """Represents a 3D distance between two points in vector form.
+
+    Two distances can be added or subtracted and distances can be
+    transformed by an angle in the xy plane.
+    """
 
     def __init__(self, x, y, z):
-        """Instantiate a Distance3D object with components x, y, and z."""
+        """Instantiate a Distance3D object with components x, y, and
+        z."""
         super().__init__(x, y)
         self.z = z
-
-    def __str__(self):
-        """Return the components of a Distance3D object."""
-        return ('x: {0.x:f}, y: {0.y:f} z: {0.z:f}'.format(self))
 
     def get_magnitude(self):
         """Return the magnitude of the Distance3D object from its
         components.
         """
-
         return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
     def get_transform(self, angle):
         """Transform the Distance3D object by an angle clockwise and
         return a new Distance3D object.
         """
+        dist = Distance(self.x, self.y)
+        dist = dist.get_transform(angle)
 
-        x = self.x * cos(angle) - self.y * sin(angle)
-        y = self.x * sin(angle) + self.y * cos(angle)
-        z = self.z
-
-        return Distance3D(x, y, z)
+        return Distance3D(dist.x, dist.y, self.z)
 
     @classmethod
     def from_magnitude(cls, dist, bearing, alt):
         """Return a new Distance object from a magnitude in the xy plane
         and a bearing.
         """
-
         return cls(dist * sin(bearing), dist * cos(bearing), alt)
+
+    def __str__(self):
+        """Return the components of a Distance3D object in a string.
+        """
+        return ('x: {0.x:f}, y: {0.y:f} z: {0.z:f}'.format(self))
